fd581cc2a8bdc44f54b4119abef75a4fb57e16db
CHavi Gupta <chavig2000@gmail.com>
2021-05-21 19:47:24 +0530

    Second Commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser=require("cookie-parser");
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://chavi_new:chavi123456@cluster0.pt5tr.mongodb.net/labDB', {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
